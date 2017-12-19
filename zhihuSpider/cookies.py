0bf625025e970b4709e0571dd632c81ae7542470
woooosz <siriussilen@hotmail.com>
2017-12-19 17:57:00 +0800

    bug fix

--- zhihuSpider/cookies.py
+++ zhihuSpider/cookies.py
@@ -24,7 +24,7 @@ def getCookies():
         r = session.post('https://www.zhihu.com/udid', headers = headers, data = {'_xsrf':xsrf})
         #拿二维码的token
         r = session.post('https://account.zhihu.com/api/login/qrcode', headers = headers, data = {'client_id':'060fc1d4872f49ec89d74814806b1c8e'})
-        s = json.loads(r.content)
+        s = json.loads(r.content.decode('utf8'))
         #生成二维码
         qrcode_terminal.draw('https://www.zhihu.com/account/scan/login/' + s['token'], 1)
         print('Plz scan your Qrcode & confirm validation in 10s...')
--- zhihuSpider/spiders/get_request.py
+++ zhihuSpider/spiders/get_request.py
@@ -19,7 +19,7 @@ class ZhihuSpider(scrapy.Spider):
 
     url_person_info_url = 'https://www.zhihu.com/api/v4/members/%s?include=locations,employments,gender,educations,business,voteup_count,thanked_Count,follower_count,following_count,cover_url,following_topic_count,following_question_count,following_favlists_count,following_columns_count,avatar_hue,answer_count,articles_count,pins_count,question_count,columns_count,commercial_question_count,favorite_count,favorited_count,logs_count,included_answers_count,included_articles_count,included_text,message_thread_token,account_status,is_active,is_bind_phone,is_force_renamed,is_bind_sina,is_privacy_protected,sina_weibo_url,sina_weibo_name,show_sina_weibo,is_blocking,is_blocked,is_following,is_followed,is_org_createpin_white_user,mutual_followees_count,vote_to_count,vote_from_count,thank_to_count,thank_from_count,thanked_count,description,hosted_live_count,participated_live_count,allow_message,industry_category,org_name,org_homepage,badge[?(type=best_answerer)].topics'
     url_person_followee_url = 'https://www.zhihu.com/api/v4/members/%s/followees?include=data[*].answer_count,articles_count,gender,follower_count,is_followed,is_following,badge[?(type=best_answerer)].topics&offset=%s&limit=20'
-    url_person_follower_url = 'http://www.zhihu.com/api/v4/members/%s/followees?limit=10&offset=%s'
+    url_person_follower_url = 'https://www.zhihu.com/api/v4/members/%s/followees?limit=10&offset=%s'
     def start_requests(self):
         url_token = 'hesenbao'
         yield scrapy.Request(
